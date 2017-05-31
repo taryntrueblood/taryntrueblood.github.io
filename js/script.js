05c8e635590d2445f7aaa6a28ac586f211b6bcbc
Taryn Trueblood <taryntrueblood@yahoo.com>
2017-05-31 09:30:22 -0500

    Added more animations

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $('#portfolio_top').click(function(){
     $('html, body').animate({scrollTop: $('#portfolio').offset().top}, 1000);
-  })
+  });
+
+  $('.title').waypoint(function(direction){
+    $('.title').addClass('animated slideInLeft');
+  }, {
+    offset: '50%'
+  });
 });
